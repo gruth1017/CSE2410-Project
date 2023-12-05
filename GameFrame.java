091d2ba3597c9cdf1f71699e8dbd87dfce4d3f8d
TariqDF <144239174+TariqDF@users.noreply.github.com>
2023-12-05 10:48:57 -0500

    Update GameFrame.java

--- GameFrame.java
+++ GameFrame.java
@@ -2,32 +2,14 @@
 import javax.swing.JFrame;
 import java.awt.BorderLayout;
 public class GameFrame extends JFrame{
-    public final int FPS = 240; // FPS CAP
-    private GamePanel gamePanel;
-    public GameFrame(String name){
+    public GameFrame(String name, GamePanel gamePanel){
         super(name);
-    }
-    public void run(LogicController controller, ChessBoard board){
-        gamePanel = new GamePanel(controller, board, FPS);
-        controller.init(gamePanel, board);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setResizable(true);
+        setResizable(false);
         setLayout(new BorderLayout());
         add(gamePanel,BorderLayout.CENTER);
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
-    // FPS LOOP DO NOT TOUCH
-    // a certain number of times per second, update all visual component, then render them.
-    // uses a dynamic measurement of time to account for processing lag.
-    long frameDelay = 1000000000/FPS;
-    long nextUpdate = System.nanoTime()+(long)frameDelay;
-     while(true){
-        if(nextUpdate<System.nanoTime()){
-            nextUpdate+=frameDelay;
-            gamePanel.update();
-            gamePanel.repaint();
-        }
-     }   
     }
 }
