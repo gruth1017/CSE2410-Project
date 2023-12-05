29b20b5424a25c1eb02712817545637a0cddd8b0
TariqDF <144239174+TariqDF@users.noreply.github.com>
2023-12-05 10:45:42 -0500

    Update Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -1,67 +1,19 @@
-public class Bishop extends ChessPiece {
-   public Bishop (boolean color, String filename) {
-      super (color, filename);
-   }
-   public boolean isLegal (ChessPiece[][] board, int y, int x, int a, int b) {
-      if (a == x && b == y) {
-         return false;
-      }
-      if (a < x && b < y) {
-         if (x - a == y - b) {
-            for (int i = 1; x - i >= a; i++) {
-               if (board[y-i][x-i] != null) {
-                  if (y - i == b && x - i == a && board[y-i][x-i].color != board[y][x].color) {
-                     return true;
-                  } else {
-                     return false;
-                  }
-               }
-            }
-            return true;
-         }
-      }
-      if (a < x && b > y) {
-         if (x - a == b - y) {
-            for (int i = 1; x - i >= a; i++) {
-               if (board[y+i][x-i] != null) {
-                  if (y + i == b && x - i == a && board[y+i][x-i].color != board[y][x].color) {
-                     return true;
-                  } else {
-                     return false;
-                  }
-               }
-            }
-            return true;
-         }
-      }
-      if (a > x && b < y) {
-         if (a - x == y - b) {
-            for (int i = 1; x + i <= a; i++) {
-               if (board[y-i][x+i] != null) {
-                  if (y - i == b && x + i == a && board[y-i][x+i].color != board[y][x].color) {
-                     return true;
-                  } else {
-                     return false;
-                  }
-               }
-            }
-            return true;
-         }
-      }
-      if (a > x && b > y) {
-         if (a - x == b - y) {
-            for (int i = 1; x + i <= a; i++) {
-               if (board[y+i][x+i] != null) {
-                  if (y + i == b && x + i == a && board[y+i][x+i].color != board[y][x].color) {
-                     return true;
-                  } else {
-                     return false;
-                  }
-               }
-            }
-            return true;
-         }
-      }
-      return false;
-   }
+import java.util.ArrayList;
+import java.awt.Point;
+public class Bishop extends ChessPiece{
+    public Bishop(Game Chess, PieceColor color, String fileName, Point position){
+        super(Chess, color, fileName, position);
+    }
+    @Override
+    public ArrayList<Point> getLegalMoves(){
+        Point p = position;
+        ArrayList<Point> legalMoves = new ArrayList<Point>();
+        // Diagonal
+        vectorPath(legalMoves, p, new Point(1,1));
+        vectorPath(legalMoves, p, new Point(1,-1));
+        vectorPath(legalMoves, p, new Point(-1,1));
+        vectorPath(legalMoves, p, new Point(-1,-1));
+        return legalMoves;
+        
+    }
 }
