6a9c200f5cbb8c8c6c0d8601a2e4922e7bfd7599
TariqDF <144239174+TariqDF@users.noreply.github.com>
2023-12-05 10:47:40 -0500

    Update ChessPiece.java

--- ChessPiece.java
+++ ChessPiece.java
@@ -3,37 +3,74 @@ import java.util.ArrayList;
 public abstract class ChessPiece { //TODO @BRADEN
     protected String fileName; 
     protected boolean hasMoved = false;
-    protected boolean color;
     protected boolean passing = false;
+    protected PieceColor color;
+    protected Game Chess;
+    Point starting_position; 
+    Point position;
    //  protected String type; Dont need; use instanceOf()
-    public ChessPiece(boolean color, String fileName){
+    public ChessPiece(Game Chess, PieceColor color, String fileName, Point position){
+        this.Chess = Chess;
         this.fileName = fileName;
         this.color = color;
+        this.position = position;
+        this.starting_position=position;
     }
+
     public String getFileName() {
         return fileName;
     }
-    protected ArrayList<Point> getLegalMoves (ChessPiece[][] board, int y, int x) {
-        ArrayList<Point> output = new ArrayList<Point>();
-        for (int i = 0; i < 8; i++) {
-           for (int j = 0; j < 8; j++) {
-              boolean legal = isLegal(board, y, x, i, j);
-              if (legal) {
-                 output.add(new Point(i,j));
-              }
-           }
-        }
-        return output;
-     }
-    public void move (GamePanel IO, ChessPiece[][] board, Point a, Point b) {
-        board[a.y][a.x] = null;
-        IO.movePiece(this, a.x, a.y, b.x, b.y);
-        board[b.y][b.x] = this;
+    public abstract ArrayList<Point> getLegalMoves ();
+    public void move (Point endPoint) {
+        Chess.board.set(position,null);
+        Chess.IO.movePiece(this, position.x, position.y, endPoint.x, endPoint.y);
+        Chess.board.set(endPoint,this);
         hasMoved = true;
+        position = endPoint;
      }
-     public String getColor() {
-      Boolean temp = color;
-      return temp.toString();
-     }
-     public abstract boolean isLegal(ChessPiece[][] board, int y, int x, int i, int j);
+    
+    // Generate all moves in a piece's path
+    protected ArrayList<Point> vectorPath(ArrayList<Point> output, Point start, Point direction){
+        while(true){
+            start = new Point(start.x+direction.x,start.y+direction.y);
+            if(outOfBounds(start)){ // if hit wall return current list
+                return output;
+            }
+            ChessPiece newPiece = Chess.board.get(start);
+            if(newPiece==null){ // if no occupying piece, is valid move keep traveling along vector in next loop
+                    output.add(start);
+                    continue;
+            }
+            // if is occupying piece, piece can not capture if own color, can capture but cant go past if different color
+            if(newPiece.color.equals(this.color)){
+                return output; 
+            }else{
+                output.add(start);
+                return output;  
+            }
+            }
+        }
+    protected boolean outOfBounds(Point p){
+        if(p.x<0 || p.y<0){
+            return true;
+        }
+        if(p.x>7 || p.y>7){
+            return true;
+        }
+        return false;
+    }
+    protected void addIfValid(ArrayList<Point> output,Point p){
+        if(outOfBounds(p)){
+            return;
+        }
+        ChessPiece newPiece = Chess.board.get(p);
+        if(newPiece==null){
+            output.add(p);
+            return;
+        }
+        if(newPiece.color.equals(this.color)){
+            return;
+        }
+        output.add(p);
+    }
     }
